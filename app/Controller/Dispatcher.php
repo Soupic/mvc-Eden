04efff838fb678e40f432168a9f7e05908991ec2
Picsou <soupic@protonmail.com>
2017-01-04 23:37:33 +0100

    Firt commit

---
+++ app/Controller/Dispatcher.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Controller;
+
+//Le dispatcher permet la création d'url propre
+
+class Dispatcher
+{
+	public function dispatch($routes, $p)
+	{
+
+		//on instancie le controller
+		$conroller = new DefaultConroller();
+		//une boucle foreach cherche la route dans le tableau
+		foreach ($routes as $url => $method) {
+			if ($url == $p) {
+				//on appel la fonction défini dans le DefaultController
+				return call_user_func([$controller, $method]);
+			}
+		}
+		//si l'URL est non définit dans les routes error404
+		return call_user_func([$controller, "error404"]);
+	}
+}
---
+++ app/config/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+//routes : URL affiché dans le navigateur
+//Dans ce tableau associative nous retrouvons en premier ce trouve l'url suivi de la fonction
+$routes = [
+	"/" => "home",
+	"/home" => "home",
+	"/news" => "news",
+	"/project" ="project",
+	"/contact" => "contact",
+];
