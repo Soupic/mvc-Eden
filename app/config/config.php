a25d6cbcaafb3ca086369fab9fa43900accfeafc
Soupic <pic.mayster@gmail.com>
2017-01-22 20:26:18 +0100

    fuck

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+	//url racine du site. Sert à générer des URLs absolues
+	const BASE_URL = "http://localhost/mvc-eden/"; 			
+	//adresse complète menant à index.php. Modifier CHEMIN ! Utile pour tous les liens dans le HTML.
+	
+
+	//infos de connexion à la db
+	const DB_HOST = "localhost";
+	const DB_NAME = "eden";
+	const DB_USER = "root";
+	const DB_PASS = "";
